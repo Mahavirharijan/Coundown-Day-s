278f6206757de65ce1e566493590c4260039e0cf
Mahavirharijan <155318827+Mahavirharijan@users.noreply.github.com>
2026-02-05 15:03:24 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   data/manifest.json
    	modified:   js/sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,25 +1,16 @@
 const CACHE_NAME = 'love-countdown-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './dashboard.html',
-  './css/style.css',
-  './js/auth.js',
-  './js/card.js',
-  './js/pwa.js',
-  './js/sw.js',
-  './js/unlock.js',
-  './data/manifest.json',
-  './data/config.json',
-  './assets/images/heart.png',
-  './assets/images/valentine.jpg',
-  './assets/images/rose.jpg',
-  './assets/images/chocolate.jpg',
-  './assets/images/teddy.jpg',
-  './assets/images/hug.jpg',
-  './assets/images/kiss.jpg',
-  './assets/images/promise.jpg',
-  './assets/images/propose.jpg'
+  '/Coundown-Day-s/',
+  '/Coundown-Day-s/index.html',
+  '/Coundown-Day-s/dashboard.html',
+  '/Coundown-Day-s/css/style.css',
+  '/Coundown-Day-s/js/auth.js',
+  '/Coundown-Day-s/js/card.js',
+  '/Coundown-Day-s/js/pwa.js',
+  '/Coundown-Day-s/js/sw.js',
+  '/Coundown-Day-s/js/unlock.js',
+  '/Coundown-Day-s/data/manifest.json',
+  '/Coundown-Day-s/data/config.json'
 ];
 
 self.addEventListener('install', (event) => {
